4f6415ef87337ec7b4cdde0733eb1ed6746cae63
Arif Gömleksiz <arifgoemleksiz101@gmail.com>
2021-05-17 12:15:20 +0200

    stupid coach

---
+++ test/system/questions_test.rb
@@ -0,0 +1,23 @@
+require 'application_system_test_case'
+
+class QuestionsTest < ApplicationSystemTestCase
+  test 'visiting /ask renders the form' do
+    visit ask_url
+    assert_selector 'h2', text: 'Ask your coach anything'
+  end
+  test 'saying Hello yields a grumpy response from the coach' do
+    visit ask_url
+    fill_in 'question', with: 'Hello'
+    click_on 'Ask'
+
+    assert_text "I don't care, get dressed and go to work!"
+  end
+  test 'Asking a question yields a dismission by the Coach' do
+    visit ask_url
+    fill_in 'question', with: 'What time is it?'
+    click_on 'Ask'
+    take_screenshot
+
+    assert_text 'Silly question, get dressed and go to work!'
+  end
+end
